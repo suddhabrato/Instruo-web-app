affaff3325a4c3a3323e51819943cb7286e78d51
Aditya Das <adityabkp1234@gmail.com>
2022-09-21 23:42:28 +0530

    auth done

--- server/app.js
+++ server/app.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const morgan = require("morgan");
 
+const userRouter = require("./routes/userRoutes");
+const AppError = require("./utils/appError");
+const globalErrorHandler = require("./controllers/errorController");
+
 const app = express();
 
 // Development Logging
@@ -8,5 +12,14 @@ if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 app.use(express.json());
+
+// Routes
+app.use("/api/v1/users", userRouter);
+
+app.all("*", (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
