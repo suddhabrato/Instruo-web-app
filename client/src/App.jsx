732711f9229fd91dc555b356e160922e26a16a8b
Devanshu Chatterjee <devanshu4190@gmail.com>
2022-10-05 19:00:49 +0530

    created basic animation for landing page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,9 +2,44 @@ import React from "react"
 
 const App = () => {
 	return (
-		<div>
-			<h1 className="text-3xl font-bold underline">Hello world!</h1>
-		</div>
+		<body>
+			<div id="main">
+				<div id="box1"></div>
+
+				<div id="box2">
+					<div id="text">
+						EXTREME <br /> UI DESIGN
+					</div>
+					<p id="para">
+						Lorem, ipsum dolor sit amet consectetur adipisicing
+						elit. Est aperiam cum perspiciatis consequatur fuga,
+						neque, nisi enim quos, amet sapiente ipsa quod
+						repellendus nesciunt dolor ea accusamus alias nam.
+						Similique delectus eum quos modi harum magnam,
+						perferendis distinctio natus assumenda ducimus quisquam
+						ab nostrum id nihil consequatur porro minima. Eaque.
+					</p>
+				</div>
+
+				<div id="box3">
+					<div id="container">
+						<div id="logo">
+							<img src="logo.png" />
+						</div>
+
+						<div id="menu">
+							<ul>
+								<li>HOME</li>
+								<li>SERVICES</li>
+								<li>ABOUT</li>
+								<li>CONTACT</li>
+								<li>HELP</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</body>
 	)
 }
 
