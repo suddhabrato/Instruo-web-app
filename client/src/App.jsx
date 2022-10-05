2903467d94a239531bfa927f1848f496d63c4ed6
intelligent-psychopath <79144740+intelligent-psychopath@users.noreply.github.com>
2022-10-05 23:02:05 +0530

    Setup basic navbar and placeholder hero

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,47 +1,130 @@
-import React from "react"
+import React from "react";
 
 const App = () => {
-	return (
-		<body>
-			<div id="main">
-				<div id="box1"></div>
+  return (
+    <>
+      <div className="navbar bg-base-300">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              <li>
+                <a>Item 1</a>
+              </li>
+              <li tabIndex={0}>
+                <a className="justify-between">
+                  Parent
+                  <svg
+                    className="fill-current"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
+                  </svg>
+                </a>
+                <ul className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                  <li>
+                    <a>Submenu 1</a>
+                  </li>
+                  <li>
+                    <a>Submenu 2</a>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <a>Item 3</a>
+              </li>
+              <li>
+                <a>Sign In</a>
+              </li>
+              <li>
+                <a>Register</a>
+              </li>
+            </ul>
+          </div>
+          <a className="btn btn-ghost normal-case text-xl">Instruo</a>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal p-0">
+            <li>
+              <a>About</a>
+            </li>
+            <li>
+              <a>Sponsors</a>
+            </li>
+            <li tabIndex={0}>
+              <a>
+                Events
+                <svg
+                  className="fill-current"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="20"
+                  height="20"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
+                </svg>
+              </a>
+              <ul className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
+                <li>
+                  <a>Submenu 1</a>
+                </li>
+                <li>
+                  <a>Submenu 2</a>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <a>Contact Us</a>
+            </li>
+          </ul>
+        </div>
+        <div className="navbar-end hidden lg:flex">
+          <a className="btn btn-outline btn-primary mx-2">Sign In</a>
+          <a className="btn btn-primary mx-2">Register</a>
+        </div>
+      </div>
+      <div
+        className="hero min-h-screen"
+        style={{
+          backgroundImage: `url("https://images.pexels.com/photos/599982/pexels-photo-599982.jpeg?auto=compress&cs=tinysrgb&w=1920&h=1080&dpr=1")`,
+        }}
+      >
+        <div className="hero-overlay bg-opacity-50"></div>
+        <div className="hero-content text-center text-neutral-content">
+          <div className="max-w-md">
+            <h1 className="mb-5 text-5xl font-bold">Instruo 2k22</h1>
+            <p className="mb-5">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi.
+            </p>
+            <button className="btn btn-primary">View Events</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-				<div id="box2">
-					<div id="text">
-						EXTREME <br /> UI DESIGN
-					</div>
-					<p id="para">
-						Lorem, ipsum dolor sit amet consectetur adipisicing
-						elit. Est aperiam cum perspiciatis consequatur fuga,
-						neque, nisi enim quos, amet sapiente ipsa quod
-						repellendus nesciunt dolor ea accusamus alias nam.
-						Similique delectus eum quos modi harum magnam,
-						perferendis distinctio natus assumenda ducimus quisquam
-						ab nostrum id nihil consequatur porro minima. Eaque.
-					</p>
-				</div>
-
-				<div id="box3">
-					<div id="container">
-						<div id="logo">
-							<img src="logo.png" />
-						</div>
-
-						<div id="menu">
-							<ul>
-								<li>HOME</li>
-								<li>ABOUT</li>
-								<li>EVENTS</li>
-								<li>SPONSORS</li>
-								<li>LOG IN</li>
-								<li>SIGN UP</li>
-							</ul>
-						</div>
-					</div>
-				</div>
-			</div>
-		</body>
-	)
-}
-
-export default App
+export default App;
