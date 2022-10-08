019d9a8d24048b45e273133d9d6104d7da236d2f
Devanshu Chatterjee <devanshu4190@gmail.com>
2022-10-08 15:22:21 +0530

    Integrated react router & switched to folder structure for pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,26 @@
-import React from "react";
-import Register from "../Register";
-import Navbar from "./Components/Navbar";
-import About from "./Components/About";
-import Hero from "./Components/Hero";
-import Footer from "./Components/Footer";
-import Sponsors from "./Components/Sponsors";
-import EventCards from "./Components/EventCards";
+import React from "react"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Register from "../Register"
+import Navbar from "./Components/Navbar"
+import About from "./Components/Home/About"
+import Hero from "./Components/Home/Hero"
+import Footer from "./Components/Footer"
+import Sponsors from "./Components/Home/Sponsors"
+import EventCards from "./Components/EventCards"
+import Home from "./Components/Home"
 
 const App = () => {
-  return (
-    <>
-      <Navbar />
-      <Hero />
-      <About />
-      {/* <EventCards /> */}
-      <Sponsors />
-      <Footer />
-    </>
-  );
-};
+	return (
+		<div>
+			<BrowserRouter>
+				<Navbar />
+				<Routes>
+					<Route path="/" element={<Home />} />
+				</Routes>
+				<Footer />
+			</BrowserRouter>
+		</div>
+	)
+}
 
-export default App;
+export default App
