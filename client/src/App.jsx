d68a8b6d70594474db9602bc74ee6d7e96104458
intelligent-psychopath <79144740+intelligent-psychopath@users.noreply.github.com>
2022-10-13 01:44:22 +0530

    Created Basic Layout for Sponsors Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,32 +1,33 @@
-import React from "react"
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import Register from "../Register"
-import Navbar from "./Components/Navbar"
-import About from "./Components/Home/About"
-import Hero from "./Components/Home/Hero"
-import Footer from "./Components/Footer"
-import Sponsors from "./Components/Home/Sponsors"
-import EventCards from "./Components/EventCards"
-import Home from "./Components/Home"
-import Events from "./Components/Events"
-import SignUp from "./Components/AuthPage/SignUp"
-import LogIn from "./Components/AuthPage/LogIn"
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Register from "../Register";
+import Navbar from "./Components/Navbar";
+import About from "./Components/Home/About";
+import Hero from "./Components/Home/Hero";
+import Footer from "./Components/Footer";
+import Sponsors from "./Components/Sponsors/Sponsors";
+import EventCards from "./Components/EventCards";
+import Home from "./Components/Home";
+import Events from "./Components/Events";
+import SignUp from "./Components/AuthPage/SignUp";
+import LogIn from "./Components/AuthPage/LogIn";
 
 const App = () => {
-	return (
-		<div>
-			<BrowserRouter>
-				<Navbar />
-				<Routes>
-					<Route path="/" element={<Home />} />
-					<Route path="/events" element={<Events />} />
-					<Route path="/signup" element={<SignUp />} />
-					<Route path="/login" element={<LogIn />} />
-				</Routes>
-				<Footer />
-			</BrowserRouter>
-		</div>
-	)
-}
+  return (
+    <div>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/sponsors" element={<Sponsors />} />
+          <Route path="/events" element={<Events />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<LogIn />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
+};
 
-export default App
+export default App;
