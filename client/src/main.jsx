b9d66c9d4ea436073e0b1b30df4e37768854543e
Devanshu Chatterjee <devanshu4190@gmail.com>
2023-01-07 10:49:25 +0530

    created context provder & added toast

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,15 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
+import { ContextProvider } from "./Contexts/ContextProvider"
+import axios from "axios"
+
 import App from "./App"
 import "./index.css"
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />)
+// axios.defaults.baseURL = ""
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<ContextProvider>
+		<App />
+	</ContextProvider>
+)
