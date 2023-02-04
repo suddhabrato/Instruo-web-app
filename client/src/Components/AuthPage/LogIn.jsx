aac405725aa3986775845e2912c0cc38c3172f6f
intelligent-psychopath <79144740+intelligent-psychopath@users.noreply.github.com>
2023-02-04 14:20:12 +0530

    responsive fix for auth pages

--- client/src/Components/AuthPage/LogIn.jsx
+++ client/src/Components/AuthPage/LogIn.jsx
@@ -1,160 +1,145 @@
-import React, { useEffect } from "react"
-import { Link, useNavigate } from "react-router-dom"
-import { Formik, Form, Field, ErrorMessage } from "formik"
-import axios from "axios"
+import React, { useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import axios from "axios";
 
-import { useStateContext } from "../../Contexts/ContextProvider"
-import HeroSection from "../Shared/HeroSection"
-import logo from "../../assets/logo-static.svg"
-import Loader from "../Shared/Loader"
+import { useStateContext } from "../../Contexts/ContextProvider";
+import HeroSection from "../Shared/HeroSection";
+import logo from "../../assets/logo-static.svg";
+import Loader from "../Shared/Loader";
 
 const LogIn = () => {
-	const { setLoginUser, showToastHandler } = useStateContext()
-	const navigate = useNavigate()
+  const { setLoginUser, showToastHandler } = useStateContext();
+  const navigate = useNavigate();
 
-	useEffect(() => {
-		if (localStorage.getItem("user")) navigate("/")
-	}, [navigate])
+  useEffect(() => {
+    if (localStorage.getItem("user")) navigate("/");
+  }, [navigate]);
 
-	return (
-		<div>
-			<HeroSection
-				title={"Login"}
-				desc={
-					"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione, ullam?"
-				}
-				breadParams={[
-					{
-						title: "Instruo",
-						url: "/",
-					},
-					{
-						title: "Login",
-						url: "",
-					},
-				]}
-			/>
+  return (
+    <div className="w-full flex flex-col">
+      <HeroSection
+        title={"Login"}
+        desc={
+          "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione, ullam?"
+        }
+        breadParams={[
+          {
+            title: "Instruo",
+            url: "/",
+          },
+          {
+            title: "Login",
+            url: "",
+          },
+        ]}
+      />
 
-			<div className="flex flex-col items-center p-5">
-				<div className="my-8 flex gap-4 justify-center">
-					<img src={logo} className="inline" width="10%" />
-					<p className="text-9xl font-bold">Instruo 2k23</p>
-				</div>
-				<div className="w-2/3 shadow-2xl bg-base-100 border-4 rounded-xl border-base-200">
-					<div className="card-body">
-						<Formik
-							initialValues={{
-								email: "",
-								password: "",
-							}}
-							validate={(values) => {
-								const errors = {}
-								if (!values.email) errors.email = "Required"
+      <div className="flex flex-col items-center my-16">
+        <div className="flex gap-4 lg:gap-8 justify-center items-center">
+          <img src={logo} className="inline" width="10%" />
+          <p className="text-4xl lg:text-8xl font-bold">Instruo 2k23</p>
+        </div>
+        <div className="lg:w-2/3 mx-4 shadow-xl bg-base-100 border-2 rounded-2xl border-base-200 mt-8">
+          <div className="card-body">
+            <Formik
+              initialValues={{
+                email: "",
+                password: "",
+              }}
+              validate={(values) => {
+                const errors = {};
+                if (!values.email) errors.email = "Required";
 
-								if (values.password.trim() === "")
-									errors.password = "Password cannot be empty"
-								return errors
-							}}
-							onSubmit={async (
-								values,
-								{ setSubmitting, resetForm }
-							) => {
-								try {
-									const { data: res } = await axios.post(
-										"/api/v1/users/login",
-										values
-									)
-									// console.log(res)
-									localStorage.setItem(
-										"user",
-										JSON.stringify({
-											...res.data.user,
-										})
-									)
-									localStorage.setItem(
-										"token",
-										res.data.token
-									)
-									
-									setLoginUser({ ...res.data.user })
+                if (values.password.trim() === "")
+                  errors.password = "Password cannot be empty";
+                return errors;
+              }}
+              onSubmit={async (values, { setSubmitting, resetForm }) => {
+                try {
+                  const { data: res } = await axios.post(
+                    "/api/v1/users/login",
+                    values
+                  );
+                  // console.log(res)
+                  localStorage.setItem(
+                    "user",
+                    JSON.stringify({
+                      ...res.data.user,
+                    })
+                  );
+                  localStorage.setItem("token", res.data.token);
 
-									setSubmitting(false)
+                  setLoginUser({ ...res.data.user });
 
-									showToastHandler(
-										"Login successful",
-										"success"
-									)
-									navigate("/")
-								} catch (error) {
-									showToastHandler("Login failed", "error")
-									console.log(error)
-									resetForm()
-									setSubmitting(false)
-								}
-							}}>
-							{({ isSubmitting }) => (
-								<Form>
-									<div className="form-control">
-										<label
-											className="label"
-											htmlFor="email">
-											Email
-										</label>
-										<Field
-											id="email"
-											type="email"
-											name="email"
-											className="input input-bordered"
-										/>
-										<ErrorMessage
-											name="email"
-											className="label text-sm text-red-500"
-											component={"div"}
-										/>
-									</div>
-									<div className="form-control">
-										<label className="label">
-											Password
-										</label>
-										<Field
-											id="password"
-											type="password"
-											name="password"
-											className="input input-bordered"
-										/>
-										<ErrorMessage
-											name="password"
-											className="label text-sm text-red-500"
-											component={"div"}
-										/>
-									</div>
+                  setSubmitting(false);
 
-									<div className="mt-6 mb-2">
-										<Link to="/signup" className="link">
-											First time user? Click here to
-											register
-										</Link>
-									</div>
+                  showToastHandler("Login successful", "success");
+                  navigate("/");
+                } catch (error) {
+                  showToastHandler("Login failed", "error");
+                  console.log(error);
+                  resetForm();
+                  setSubmitting(false);
+                }
+              }}
+            >
+              {({ isSubmitting }) => (
+                <Form>
+                  <div className="form-control">
+                    <label className="label" htmlFor="email">
+                      Email
+                    </label>
+                    <Field
+                      id="email"
+                      type="email"
+                      name="email"
+                      className="input input-bordered"
+                    />
+                    <ErrorMessage
+                      name="email"
+                      className="label text-sm text-red-500"
+                      component={"div"}
+                    />
+                  </div>
+                  <div className="form-control">
+                    <label className="label">Password</label>
+                    <Field
+                      id="password"
+                      type="password"
+                      name="password"
+                      className="input input-bordered"
+                    />
+                    <ErrorMessage
+                      name="password"
+                      className="label text-sm text-red-500"
+                      component={"div"}
+                    />
+                  </div>
 
-									<div className="form-control mt-6 w-40 mx-auto">
-										{!isSubmitting ? (
-											<button
-												type="submit"
-												className="btn btn-primary">
-												Login
-											</button>
-										) : (
-											<Loader />
-										)}
-									</div>
-								</Form>
-							)}
-						</Formik>
-					</div>
-				</div>
-			</div>
-		</div>
-	)
-}
+                  <div className="mt-6 mb-2">
+                    <Link to="/signup" className="link">
+                      First time user? Click here to register
+                    </Link>
+                  </div>
 
-export default LogIn
+                  <div className="form-control mt-6 w-40 mx-auto">
+                    {!isSubmitting ? (
+                      <button type="submit" className="btn btn-primary">
+                        Login
+                      </button>
+                    ) : (
+                      <Loader />
+                    )}
+                  </div>
+                </Form>
+              )}
+            </Formik>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LogIn;
