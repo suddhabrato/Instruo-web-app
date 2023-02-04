c251c2605ba07a5b81f02d430d924d52026d1b4e
intelligent-psychopath <79144740+intelligent-psychopath@users.noreply.github.com>
2023-02-04 15:03:59 +0530

    Added Footer Links and scrollToTop on Route Change

--- client/src/Components/Shared/Footer.jsx
+++ client/src/Components/Shared/Footer.jsx
@@ -1,56 +1,9 @@
 import React from "react";
 import logo from "../../assets/logo-static.svg";
+import { Link } from "react-router-dom";
+
 const Footer = () => {
   return (
-    // <footer className="footer footer-center p-10 bg-neutral text-neutral-content rounded">
-    //   <div className="grid grid-flow-col gap-4">
-    //     <a className="link link-hover">About</a>
-    //     <a className="link link-hover">Contact</a>
-    //     <a className="link link-hover">Events</a>
-    //     <a className="link link-hover">Workshops</a>
-    //     <a className="link link-hover">Sponsors</a>
-    //   </div>
-    //   <div>
-    //     <div className="grid grid-flow-col gap-4">
-    //       <a>
-    //         <svg
-    //           xmlns="http://www.w3.org/2000/svg"
-    //           width="24"
-    //           height="24"
-    //           viewBox="0 0 24 24"
-    //           className="fill-current"
-    //         >
-    //           <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path>
-    //         </svg>
-    //       </a>
-    //       <a>
-    //         <svg
-    //           xmlns="http://www.w3.org/2000/svg"
-    //           width="24"
-    //           height="24"
-    //           viewBox="0 0 24 24"
-    //           className="fill-current"
-    //         >
-    //           <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
-    //         </svg>
-    //       </a>
-    //       <a>
-    //         <svg
-    //           xmlns="http://www.w3.org/2000/svg"
-    //           width="24"
-    //           height="24"
-    //           viewBox="0 0 24 24"
-    //           className="fill-current"
-    //         >
-    //           <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
-    //         </svg>
-    //       </a>
-    //     </div>
-    //   </div>
-    //   <div>
-    //     <p>Copyright © 2022 - All right reserved by Instruo 2k22</p>
-    //   </div>
-    // </footer>
     <>
       <footer className="footer p-16 pb-12 bg-neutral text-base-content">
         <div className="max-w-md">
@@ -63,17 +16,33 @@ const Footer = () => {
         </div>
         <div>
           <span className="footer-title">Explore</span>
-          <a className="link link-hover">Home</a>
-          <a className="link link-hover">Sponsors</a>
-          <a className="link link-hover">Contacts</a>
-          <a className="link link-hover">Our Team</a>
+          <Link to="/" className="link link-hover">
+            Home
+          </Link>
+          <Link to="/sponsors" className="link link-hover">
+            Sponsors
+          </Link>
+          <Link to="/contactus" className="link link-hover">
+            Contacts
+          </Link>
+          <Link to="/contactus" className="link link-hover">
+            Our Team
+          </Link>
         </div>
         <div>
           <span className="footer-title">Join Us</span>
-          <a className="link link-hover">Events</a>
-          <a className="link link-hover">Workshops</a>
-          <a className="link link-hover">Register</a>
-          <a className="link link-hover">Sign In</a>
+          <Link to="/events" className="link link-hover">
+            Events
+          </Link>
+          <Link to="/workshops" className="link link-hover">
+            Workshops
+          </Link>
+          <Link to="/signup" className="link link-hover">
+            Sign Up
+          </Link>
+          <Link to="/login" className="link link-hover">
+            Sign In
+          </Link>
         </div>
         <div>
           <span className="footer-title">Get in Touch with us</span>
@@ -164,31 +133,8 @@ const Footer = () => {
         </div>
         <div>
           <span className="footer-title">Socials</span>
-          <div className="grid grid-flow-col gap-4">
-            {/* Twitter, Youtube */}
-            {/* <a>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                className="fill-current"
-              >
-                <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path>
-              </svg>
-            </a>
-            <a>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                className="fill-current"
-              >
-                <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
-              </svg>
-            </a> */}
-            <a href="https://www.facebook.com/instruo.iiests">
+          <div className="flex items-center justify-center gap-4">
+            <a href="https://www.facebook.com/instruo.iiests" target="_blank">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="24"
@@ -199,7 +145,7 @@ const Footer = () => {
                 <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
               </svg>
             </a>
-            <a href="https://www.instagram.com/instruo.iiests/">
+            <a href="https://www.instagram.com/instruo.iiests/" target="_blank">
               <svg
                 width="28"
                 height="28"
