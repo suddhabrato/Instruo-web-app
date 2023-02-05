94d12561ff56d4b1874fbe832ce0d24555be9603
Devanshu Chatterjee <devanshu4190@gmail.com>
2023-02-05 11:29:39 +0530

    added flow for workshop registration & updated profile page to show registered workshops

--- client/src/Components/Workshops/WorkshopSingle/index.jsx
+++ client/src/Components/Workshops/WorkshopSingle/index.jsx
@@ -4,23 +4,42 @@ import WorkshopDetails from "./WorkshopDetails"
 import HeroSection from "../../Shared/HeroSection"
 import axios from "axios"
 import Loader from "../../Shared/Loader"
+import { useStateContext } from "../../../Contexts/ContextProvider"
 
 const WorkshopSingle = ({ workshopId }) => {
-	const [workshop, getWorkshop] = useState()
+	const { loginUser } = useStateContext()
+
+	const [workshop, setWorkshop] = useState("")
+	const [isRegistered, setIsRegistered] = useState(false)
 	const [loading, setLoading] = useState(true)
+
+	const getWorkshopDetails = async () => {
+		try {
+			let res
+			if (loginUser)
+				({ data: res } = await axios.post(
+					`/api/v1/workshops/${workshopId}`,
+					{
+						userId: loginUser._id,
+					}
+				))
+			else
+				({ data: res } = await axios.post(
+					`/api/v1/workshops/${workshopId}`
+				))
+			console.log(res)
+			setWorkshop(res.data.workshop)
+			setIsRegistered(res.data.isRegistered)
+			setLoading(false)
+		} catch (error) {
+			console.log(error)
+		}
+	}
+
 	useEffect(() => {
-		axios
-			.get(`/api/v1/workshops/${workshopId}`, {
-				headers: {
-					Accept: "application/json",
-				},
-			})
-			.then((res) => {
-				console.log(res.data)
-				getWorkshop(res.data.data.workshop)
-				setLoading(false)
-			})
+		getWorkshopDetails()
 	}, [])
+
 	return (
 		<div>
 			{loading ? (
@@ -52,7 +71,7 @@ const WorkshopSingle = ({ workshopId }) => {
 						<h2 className="mb-8 text-5xl font-bold text-center">
 							Registration Status
 						</h2>
-						<Register />
+						<Register {...workshop} isRegistered={isRegistered} />
 					</div>
 				</div>
 			)}
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,22 +1,22 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { ContextProvider } from "./Contexts/ContextProvider";
-import { BrowserRouter } from "react-router-dom";
-import axios from "axios";
-import App from "./App";
-import "./index.css";
-import ScrollToTop from "./Utils/scrollToTop";
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { ContextProvider } from "./Contexts/ContextProvider"
+import { BrowserRouter } from "react-router-dom"
+import axios from "axios"
+import App from "./App"
+import "./index.css"
+import ScrollToTop from "./Utils/scrollToTop"
 
-axios.defaults.baseURL = "https://instruo-server.onrender.com";
-// axios.defaults.baseURL = "http://localhost:3000"
-axios.defaults.headers.common["Access-Control-Allow-Origin"] = "*";
-axios.defaults.headers.common["Access-Control-Allow-Headers"] = "*";
+// axios.defaults.baseURL = "https://instruo-server.onrender.com";
+axios.defaults.baseURL = "http://localhost:3000"
+axios.defaults.headers.common["Access-Control-Allow-Origin"] = "*"
+axios.defaults.headers.common["Access-Control-Allow-Headers"] = "*"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <ContextProvider>
-    <BrowserRouter>
-      <ScrollToTop />
-      <App />
-    </BrowserRouter>
-  </ContextProvider>
-);
+	<ContextProvider>
+		<BrowserRouter>
+			<ScrollToTop />
+			<App />
+		</BrowserRouter>
+	</ContextProvider>
+)
