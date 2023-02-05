6b38b84d48dcdb58f7fad6ddca7b3964d6b2d918
intelligent-psychopath <79144740+intelligent-psychopath@users.noreply.github.com>
2023-02-05 11:42:06 +0530

    Added Rounds For Events

--- client/src/Components/Events/EventSingle/EventDetails.jsx
+++ client/src/Components/Events/EventSingle/EventDetails.jsx
@@ -5,24 +5,26 @@ import About from "./About";
 import Schedule from "./Schedule";
 import Prizes from "./Prizes";
 import Contacts from "./Contacts";
+import Rounds from "./Rounds";
 
 const EventDetails = ({ desc, rules, schedule, contacts, faq, prizes }) => {
-	return (
-		<div>
-			<div className="bg-base-100 text-base-content mx-auto max-w-md md:max-w-full px-8">
-				<div className="hero-content px-4 md:px-2 mx-auto flex flex-col">
-					<div className="prose w-full max-w-full flex-grow px-4 md:px-2 my-20">
-						<About desc={desc} />
-						<Rules rules={rules} />
-						<Schedule schedule={schedule} />
-						{/* <Faq faq={faq} /> */}
-						<Contacts contacts={contacts} />
-						<Prizes prizes={prizes} />
-					</div>
-				</div>
-			</div>
-		</div>
-	)
-}
+  return (
+    <div>
+      <div className="bg-base-100 text-base-content mx-auto max-w-md md:max-w-full px-8">
+        <div className="hero-content px-4 md:px-2 mx-auto flex flex-col">
+          <div className="prose w-full max-w-full flex-grow px-4 md:px-2 my-20">
+            <About desc={desc} />
+            <Rules rules={rules} />
+            <Rounds schedule={schedule} />
+            <Schedule schedule={schedule} />
+            {/* <Faq faq={faq} /> */}
+            <Contacts contacts={contacts} />
+            <Prizes prizes={prizes} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default EventDetails;
