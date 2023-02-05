0765c833a35d8c83d568bac742a4db48a3efeb5e
intelligent-psychopath <79144740+intelligent-psychopath@users.noreply.github.com>
2023-02-05 18:58:49 +0530

    Minor Styling fixes and content Addition T Shirt

--- client/src/Components/ContactUs/index.jsx
+++ client/src/Components/ContactUs/index.jsx
@@ -1,95 +1,98 @@
-import React, { useState } from "react"
+import React, { useState } from "react";
 
-import HeroSection from "../Shared/HeroSection"
-import ContactCard from "./ContactCard"
+import HeroSection from "../Shared/HeroSection";
+import ContactCard from "./ContactCard";
 
-import devanshu from "../../assets/team/devanshu.jpg"
-import sudo from "../../assets/team/sudo.jpeg"
-import debopriya from "../../assets/team/debopriya.jpeg"
-import aditya from "../../assets/team/aditya.jpeg"
+import devanshu from "../../assets/team/devanshu.jpg";
+import sudo from "../../assets/team/sudo.jpeg";
+import debopriya from "../../assets/team/debopriya.jpeg";
+import aditya from "../../assets/team/aditya.jpeg";
 
 const ContactUs = () => {
-	const [tabActive, setTabActive] = useState(2)
-	return (
-		<div>
-			<HeroSection
-				title={"Contact Us"}
-				desc={"The team behind Instruo 12.0"}
-				breadParams={[
-					{
-						title: "Instruo",
-						url: "/",
-					},
-					{
-						title: "Contact Us",
-						url: "",
-					},
-				]}
-			/>
-			<div className="tabs tabs-boxed flex justify-center">
-				<a
-					className={`tab ${tabActive === 1 ? "tab-active" : ""}`}
-					onClick={() => setTabActive(1)}>
-					Design Team
-				</a>
-				<a
-					className={`tab ${tabActive === 2 ? "tab-active" : ""}`}
-					onClick={() => setTabActive(2)}>
-					Technical Team
-				</a>
-				<a
-					className={`tab ${tabActive === 3 ? "tab-active" : ""}`}
-					onClick={() => setTabActive(3)}>
-					Management Team
-				</a>
-			</div>
-			{tabActive === 1 && (
-				<div className="flex flex-wrap gap-10 p-10 justify-center">
-					<ContactCard />
-					<ContactCard />
-					<ContactCard />
-				</div>
-			)}
-			{tabActive === 2 && (
-				<div className="flex flex-wrap gap-10 p-10 justify-center">
-					<ContactCard
-						avatarImg={devanshu}
-						name="Devanshu Chatterjee"
-						role="Website Developer"
-						linkedinUrl="https://www.linkedin.com/in/devanshu-chatterjee-517645200/"
-					/>
-					<ContactCard
-						avatarImg={sudo}
-						name="Suddhabrato Ghosh"
-						role="Website Developer"
-						linkedinUrl="https://www.linkedin.com/in/suddhabrato-ghosh/"
-					/>
-					<ContactCard
-						avatarImg={debopriya}
-						name="Debopriya Roy"
-						role="Website Developer"
-						linkedinUrl="http://www.linkedin.com/in/debopriya-roy"
-					/>
-					<ContactCard
-						avatarImg={aditya}
-						name="Aditya Das"
-						role="Website Developer"
-						linkedinUrl="https://www.linkedin.com/in/aditya-das-86069b202/"
-					/>
-				</div>
-			)}
-			{tabActive === 3 && (
-				<div className="flex flex-wrap gap-10 p-10 justify-center">
-					<ContactCard />
-					<ContactCard />
-					<ContactCard />
-					<ContactCard />
-					<ContactCard />
-					<ContactCard />
-				</div>
-			)}
-		</div>
-	)
-}
+  const [tabActive, setTabActive] = useState(2);
+  return (
+    <div>
+      <HeroSection
+        title={"Contact Us"}
+        desc={"The team behind Instruo 12.0"}
+        breadParams={[
+          {
+            title: "Instruo",
+            url: "/",
+          },
+          {
+            title: "Contact Us",
+            url: "",
+          },
+        ]}
+      />
+      <div className="tabs tabs-boxed flex justify-center">
+        <a
+          className={`tab ${tabActive === 1 ? "tab-active" : ""}`}
+          onClick={() => setTabActive(1)}
+        >
+          Design Team
+        </a>
+        <a
+          className={`tab ${tabActive === 2 ? "tab-active" : ""}`}
+          onClick={() => setTabActive(2)}
+        >
+          Technical Team
+        </a>
+        <a
+          className={`tab ${tabActive === 3 ? "tab-active" : ""}`}
+          onClick={() => setTabActive(3)}
+        >
+          Management Team
+        </a>
+      </div>
+      {tabActive === 1 && (
+        <div className="flex flex-wrap gap-10 p-10 justify-center">
+          <ContactCard />
+          <ContactCard />
+          <ContactCard />
+        </div>
+      )}
+      {tabActive === 2 && (
+        <div className="flex flex-wrap gap-10 p-10 justify-center">
+          <ContactCard
+            avatarImg={devanshu}
+            name="Devanshu Chatterjee"
+            role="Website Developer"
+            linkedinUrl="https://www.linkedin.com/in/devanshu-chatterjee-517645200/"
+          />
+          <ContactCard
+            avatarImg={sudo}
+            name="Suddhabrato Ghosh"
+            role="Website Developer"
+            linkedinUrl="https://www.linkedin.com/in/suddhabrato-ghosh/"
+          />
+          <ContactCard
+            avatarImg={aditya}
+            name="Aditya Das"
+            role="Website Developer"
+            linkedinUrl="https://www.linkedin.com/in/aditya-das-86069b202/"
+          />
+          <ContactCard
+            avatarImg={debopriya}
+            name="Debopriya Roy"
+            role="Website Developer"
+            linkedinUrl="http://www.linkedin.com/in/debopriya-roy"
+          />
+        </div>
+      )}
+      {tabActive === 3 && (
+        <div className="flex flex-wrap gap-10 p-10 justify-center">
+          <ContactCard />
+          <ContactCard />
+          <ContactCard />
+          <ContactCard />
+          <ContactCard />
+          <ContactCard />
+        </div>
+      )}
+    </div>
+  );
+};
 
-export default ContactUs
+export default ContactUs;
--- client/src/Components/Events/EventSingle/index.jsx
+++ client/src/Components/Events/EventSingle/index.jsx
@@ -1,84 +1,80 @@
-import { React, useState, useEffect } from "react"
-import Register from "./Register"
-import EventDetails from "./EventDetails"
-import HeroSection from "../../Shared/HeroSection"
-import axios from "axios"
-import Loader from "../../Shared/Loader"
-import { useStateContext } from "../../../Contexts/ContextProvider"
+import { React, useState, useEffect } from "react";
+import Register from "./Register";
+import EventDetails from "./EventDetails";
+import HeroSection from "../../Shared/HeroSection";
+import axios from "axios";
+import Loader from "../../Shared/Loader";
+import { useStateContext } from "../../../Contexts/ContextProvider";
 
 const EventSingle = ({ eventId }) => {
-	const { loginUser } = useStateContext()
+  const { loginUser } = useStateContext();
 
-	const [event, setEvent] = useState("")
-	const [team, setTeam] = useState("")
-	const [isRegistered, setIsRegistered] = useState(false)
-	const [loading, setLoading] = useState(true)
+  const [event, setEvent] = useState("");
+  const [team, setTeam] = useState("");
+  const [isRegistered, setIsRegistered] = useState(false);
+  const [loading, setLoading] = useState(true);
 
-	const getEventDetails = async () => {
-		try {
-			let res
-			if (loginUser)
-				({ data: res } = await axios.post(`/api/v1/events/${eventId}`, {
-					userId: loginUser._id,
-				}))
-			else ({ data: res } = await axios.post(`/api/v1/events/${eventId}`))
-			// console.log(res)
-			setEvent(res.data.event)
-			setTeam(res.data.team)
-			setIsRegistered(res.data.isRegistered)
-			setLoading(false)
-		} catch (error) {
-			console.log(error)
-		}
-	}
+  const getEventDetails = async () => {
+    try {
+      let res;
+      if (loginUser)
+        ({ data: res } = await axios.post(`/api/v1/events/${eventId}`, {
+          userId: loginUser._id,
+        }));
+      else ({ data: res } = await axios.post(`/api/v1/events/${eventId}`));
+      // console.log(res)
+      setEvent(res.data.event);
+      setTeam(res.data.team);
+      setIsRegistered(res.data.isRegistered);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-	useEffect(() => {
-		getEventDetails()
-	}, [])
+  useEffect(() => {
+    getEventDetails();
+  }, []);
 
-	return (
-		<div>
-			{loading ? (
-				<div className="text-center p-10">
-					<Loader />
-				</div>
-			) : (
-				<div>
-					<HeroSection
-						title={event.title}
-						desc={event.subtitle}
-						breadParams={[
-							{
-								title: "Instruo",
-								url: "/",
-							},
-							{
-								title: "Events",
-								url: "/events",
-							},
-							{
-								title: event.title,
-								url: "",
-							},
-						]}
-					/>
-					<div>
-						<EventDetails {...event} />
-						<div className="">
-							<h2 className="mb-8 text-5xl font-bold text-center">
-								Registration Status
-							</h2>
-							<Register
-								{...event}
-								team={team}
-								isRegistered={isRegistered}
-							/>
-						</div>
-					</div>
-				</div>
-			)}
-		</div>
-	)
-}
+  return (
+    <div>
+      {loading ? (
+        <div className="text-center p-10">
+          <Loader />
+        </div>
+      ) : (
+        <div>
+          <HeroSection
+            title={event.title}
+            desc={event.subtitle}
+            breadParams={[
+              {
+                title: "Instruo",
+                url: "/",
+              },
+              {
+                title: "Events",
+                url: "/events",
+              },
+              {
+                title: event.title,
+                url: "",
+              },
+            ]}
+          />
+          <div>
+            <EventDetails {...event} />
+            <div className="mb-8">
+              <h2 className="mb-8 text-5xl font-bold text-center">
+                Registration Status
+              </h2>
+              <Register {...event} team={team} isRegistered={isRegistered} />
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
 
-export default EventSingle
+export default EventSingle;
