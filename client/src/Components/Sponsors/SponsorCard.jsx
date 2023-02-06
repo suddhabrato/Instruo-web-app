24970fe052d74eadd36bcaaf830225ecacefe8d3
Devanshu Chatterjee <devanshu4190@gmail.com>
2023-02-06 14:20:10 +0530

    removed links from sponsor card

--- client/src/Components/Sponsors/SponsorCard.jsx
+++ client/src/Components/Sponsors/SponsorCard.jsx
@@ -2,18 +2,14 @@ import React from "react"
 
 const SponsorCard = ({ name, logoUrl }) => {
 	return (
-		<a
-			className="card border-2 border-base-200 card-compact bg-white/5 hover:bg-gray-300/10 transition-all duration-200 hover:shadow hover:-translate-y-1"
-			href="https://stackblitz.com/edit/daisyui-vite"
-			target="_blank"
-			rel="noopener">
+		<div className="card border-2 border-base-200 card-compact bg-white/5 hover:bg-gray-300/10 transition-all duration-200 hover:shadow hover:-translate-y-1">
 			<figure className="px-12 pt-6 pb-2 w-full h-full aspect-[2/1] items-end">
 				<img className="w-full" src={logoUrl} alt={name} />
 			</figure>
 			<div className="card-body text-center">
 				<span className="md:text-xl">{name}</span>
 			</div>
-		</a>
+		</div>
 	)
 }
 
